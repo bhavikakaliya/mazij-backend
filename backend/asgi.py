3626b7ada0d810d1be10a4d3bb3098c949500af5
lolmokha <mkhan0138@gmail.com>
2022-03-20 16:49:17 +0400

    minor edit, might fix

--- backend/asgi.py
+++ backend/asgi.py
@@ -6,7 +6,7 @@ from django.core.asgi import get_asgi_application
 
 import channels_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
